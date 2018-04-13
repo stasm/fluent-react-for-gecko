306cdf1fafddaf9d9a78e969ae99baf9b675eead
Staś Małolepszy <stas@mozilla.com>
2018-04-13 17:38:21 +0200

    GeckoLocalizationProvider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import GeckoLocalizationProvider from './l10n';
+import GeckoLocalizationProvider from './GeckoLocalizationProvider';
 import App from './App';
 
 ReactDOM.render(
